07311104d6ef46ec0fd5aa7438937e1f2e43b15e
Marco Antonio Islas Cruz <markuz@islascruz.org>
2014-04-16 14:06:28 -0500

    Update year in license statement.

--- mkzgdrive.py
+++ mkzgdrive.py
@@ -4,7 +4,7 @@
 #
 # This file is part of mkzgdrive project
 #
-# Copyright (c) 2011 Marco Antonio Islas Cruz
+# Copyright (c) 2014 Marco Antonio Islas Cruz
 #
 # This script is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -21,7 +21,7 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 #
 # @author    Marco Antonio Islas Cruz <markuz@islascruz.org>
-# @copyright 2011 Marco Antonio Islas Cruz
+# @copyright 2014 Marco Antonio Islas Cruz
 # @license   http://www.gnu.org/licenses/gpl.txt
 
 
